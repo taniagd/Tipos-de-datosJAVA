4293fa313e57dfaf1ccee0b68e0875151f5ce409
Tania Gayosso <taniagd09@gmail.com>
2021-07-12 02:05:04 -0500

    ¡Hola Mundo! y tipos de datos en Java

---
+++ src/TiposDatos.java
@@ -0,0 +1,36 @@
+
+public class TiposDatos {
+
+	public static void main(String[] args) { /*main es el método de entrada*/
+		// TODO Auto-generated method stub
+
+		System.out.println("¡Hola Mundo!");
+		
+		//Para declarar variables se debe especificar tipo de dato
+		int numero1 = 30; 
+		int numero2 = 6;
+		int multiplicacion = numero1 * numero2;
+		
+		System.out.println(multiplicacion); 
+		//Se imprime el resultado de la operación declarada en la línea 12
+		
+		String cadena1 = "texto1";
+		String cadena2 = "texto2";
+		char letra = 'y';
+		//Se usan comillas simples solo cuando es un carácter.
+		
+		String oracion = cadena1 + cadena2;
+		
+		boolean mayor18 = true;
+		
+		System.out.println(oracion);
+		//Se imprime lo declarado en línea 20.
+		
+		
+		
+	
+	}
+	
+
+}
+
